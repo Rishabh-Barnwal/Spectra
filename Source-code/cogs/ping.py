1a36de8e1927d6f4615ce775c1049efcc205b0aa
Rishabh-Barnwal <rishabhbarnwalmc@gmail.com>
2024-11-25 18:25:29 +0530

    debugged the code

--- Source-code/cogs/ping.py
+++ Source-code/cogs/ping.py
@@ -13,7 +13,7 @@ class ping(commands.Cog):
 
     @app_commands.command(name = "ping", description = "Sends the time it takes your message to reach spectra!")
     async def  ping(self, interaction: discord.Interaction): #The parameters are basically context (or the command) and length of the password (Default is 12)
-        latency = ctx.bot.latency*1000
+        latency = interaction.client.latency*1000
         await interaction.response.send_message(f"Latency: {latency:.2f}")
 
 async def setup(bot):
