1d8729f82508b0108d6aebf86b5346a3fa94eb52
Rishabh-Barnwal <rishabhbarnwalmc@gmail.com>
2024-11-25 20:18:56 +0530

    Added comments to /ping command

--- Source-code/cogs/ping.py
+++ Source-code/cogs/ping.py
@@ -1,20 +1,21 @@
+# The modules we are going to require for the ping command
 import discord
 from discord.ext import commands
 from discord import app_commands
-
-class ping(commands.Cog):
-    def __init__(self, bot):
+# Definition of a class named ping
+class ping(commands.Cog): # Links the command file to the command handler
+    def __init__(self, bot): # Initializes the bot to itself
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_ready(self):
+    @commands.Cog.listener() # Registers a listener for events (enables the cog to respond to Discord events)
+    async def on_ready(self): # This one is just for confirming that the command has been loaded (Prints it in terminal itself)
         print('Ping loaded')
 
 
-    @app_commands.command(name = "ping", description = "Sends the time it takes your message to reach spectra!")
-    async def  ping(self, interaction: discord.Interaction): #The parameters are basically context (or the command) and length of the password (Default is 12)
-        latency = interaction.client.latency*1000
-        await interaction.response.send_message(f"Latency: {latency:.2f}ms")
+    @app_commands.command(name = "ping", description = "Sends the time it takes your message to reach spectra!") # The bot is initialized to command /calc
+    async def  ping(self, interaction: discord.Interaction): #The parameters are basically the bot initialisation and the location of interaction with the bot
+        latency = interaction.client.latency*1000 #The actual latency extractor
+        await interaction.response.send_message(f"Latency: {latency:.2f}ms") #Sends the message
 
-async def setup(bot):
+async def setup(bot): #Marks the end of the command and adds it to a folder called _pycache_
     await bot.add_cog(ping(bot))
