0bf57adb1266af27db652268eeff2f6a1df8cbf0
Rishabh-Barnwal <rishabhbarnwalmc@gmail.com>
2024-12-17 17:27:23 +0530

    Fixed errors

--- Source-code/cogs/r-img.py
+++ Source-code/cogs/r-img.py
@@ -31,7 +31,7 @@ class Rimg(commands.Cog):  # Links the command file to the command handler
         try:
             # Fetch subreddit posts
             subreddit = await self.reddit.subreddit("LandscapePhotography")
-            posts = [post async for post in subreddit(limit=100)]  # Fetch top 100 hot posts asynchronously
+            posts = [post async for post in subreddit.hot(limit=100)]  # Fetch top 100 hot posts asynchronously
 
             # Choose a random post
             random_post = random.choice(posts)
