a353fd3b2a3bc40881baa32f1efbb384e182f164
Rishabh-Barnwal <rishabhbarnwalmc@gmail.com>
2025-01-05 17:51:44 +0530

    Changed comments

--- Source-code/command handler.py
+++ Source-code/command handler.py
@@ -1,28 +1,28 @@
-# The modules we are going to require for the command handler
+#The modules we are going to require for the command handler
 import discord
 import asyncio
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
-# Message's intents are initialized (defining what events the bot can respond to)
-intents = discord.Intents.all()  # Enables all intents
-intents.message_content = True  # Allows the bot to read the content of the message
-# Bot is initialized to the command with prefix [!]
+#Message's intents are initialized (defining what events the bot can respond to)
+intents = discord.Intents.all()  #Enables all intents
+intents.message_content = True  #Allows the bot to read the content of the message
+#Bot is initialized to the command with prefix [!]
 bot = commands.Bot(command_prefix='!', intents=intents)
-# Bot is initialized to command !sync (Required during development phase)
-@bot.command()  # Changed to @bot.command() to make it work only with `!sync`
+#Bot is initialized to command !sync (Required during development phase)
+@bot.command()
 async def sync(ctx: commands.Context):
     x = await ctx.bot.tree.sync()
     await ctx.send(f'Synced {len(x)} commands')
-# Loads all the created commands ending with extension .py
+#Loads all the created commands ending with extension .py
 async def load():
-    for filename in os.listdir('./Source-code/cogs'):  # Checks for file with path /cogs
+    for filename in os.listdir('./Source-code/cogs'):  #Checks for file with path /Source-code/cogs
         if filename.endswith('.py'):
-            # index -3 for the last ".py" of file
-            await bot.load_extension(f'cogs.{filename[:-3]}')  # Loads the command files
-async def cmdhandler():  # Asynchronous function called command handler is defined 
+            #index -3 for the last ".py" of file
+            await bot.load_extension(f'cogs.{filename[:-3]}')  #Loads the command files
+async def cmdhandler():  #Asynchronous function called command handler is defined 
     load_dotenv()
-    Bot_Token = os.getenv("DISCORD_TOKEN")
+    Bot_Token = os.getenv("DISCORD_TOKEN") #Fetches the bot token from the .env file
     await load()
-    await bot.start(f"{Bot_Token}")  # Your actual bot token
-asyncio.run(cmdhandler())  # Starts the command handler bot asynchronously
+    await bot.start(f"{Bot_Token}")  #Starts the bot with the token
+asyncio.run(cmdhandler())  #Starts the command handler bot asynchronously
